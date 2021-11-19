4a2491d30a28a0bfa80be3350c8b2c079104d211
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2021-11-19 21:51:12 +0300

    Update string_func.cpp

--- string_func.cpp
+++ string_func.cpp
@@ -69,46 +69,6 @@ while(itc_isFirstInSecond("  ", str)){
 return str;
 }
 
-bool itc_isIp(string str){
-int i = 0, f;
-long long num;
-string sup = "";
-if (itc_len)
-while (itc_isFirstInSecond(".", str)){
-        //                                                                                                    cout << "\n"<<endl;
-    f = itc_find_str(str, ".");
-    //                                                                                                    cout << f << endl;
-    if (f > 1){sup = itc_slice_str(str, 0, f - 1);}
-    else if (f == 1) {sup = +str[0];}
-    //                                                                                                    cout << sup << endl;
-    //                                                                                                    cout << "len = " << itc_len(sup) << endl;
-    //                                                                                                    cout << "max_char = " << itc_max_char_on_end(sup) << endl;
-    if (sup_3raz_number(sup)){
-        //                                                                                                    cout << "in if ______" << endl;
-        if (sup != "0"){num = string_number(sup);}
-        else if( sup == "0"){num = 0;}
-        //                                                                                                    cout << num << endl;
-        if (num >= 0 && num <= 255){
-            i++;
-        }
-    }
-    str = itc_slice_str(str, f + 1, itc_len(str));
-    //                                                                                                    cout << "str" << str << endl;
-}
-if (sup_3raz_number(str)){
-        //                                                                                                    cout << "in if ______" << endl;
-    if (str != "0"){num = string_number(str);}
-    else if( str == "0"){num = 0;}
-        //                                                                                                    cout << num << endl;
-    if (num >= 0 && num <= 255){
-        i++;
-    }
-}
-if (i == 4){
-    return true;
-}
-return false;
-}
 
 
 string itc_decToBase(int num, int base){
