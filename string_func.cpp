42630a75ec4649abf35229b45582e424888091d6
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2021-11-25 21:00:43 +0300

    Update string_func.cpp

--- string_func.cpp
+++ string_func.cpp
@@ -32,79 +32,24 @@ string itc_decToBase(int num, int base){
 string str;
 int a = 0;
 while (num > 0){
-
 a = num % base;
-
-if (a == 0){
-    str += '0';
-}
-
-else if (a == 1){
-    str += '1';
-}
-
-else if (a == 2){
-    str += '2';
-}
-
-else if (a == 3){
-    str += '3';
-}
-
-else if (a == 4){
-    str += '4';
-}
-
-else if (a == 5){
-    str += '5';
-}
-
-else if (a == 6){
-    str += '6';
-}
-
-else if (a == 7){
-    str += '7';
-}
-
-else if (a == 8){
-    str += '8';
-}
-
-else if (a == 9){
-    str += '9';
-}
-
-else if (a == 10){
+if (a < 10)
+    str += char_number(a);
+else if (a == 10)
     str += 'A';
-}
-
-else if (a == 11){
+else if (a == 11)
     str += 'B';
-}
-
-else if (a == 12){
+else if (a == 12)
     str += 'C';
-}
-
-else if (a == 13){
+else if (a == 13)
     str += 'D';
-}
-
-else if (a == 14){
+else if (a == 14)
     str += 'E';
-}
-
-else if (a == 15){
+else if (a == 15)
     str += 'F';
-}
-
 num = num / base;
-
 }
-
 str = itc_reverse_str(str);
-
 return str;
 }
 
